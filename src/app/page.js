f8db167efc08f835cd5aba5de0f1ea76127c1853
JuanesVasco19 <juan.vascol@upb.edu.co>
2025-05-14 17:43:32 -0500

    arrepentimiento frontend otra vez

--- src/app/page.js
+++ src/app/page.js
@@ -1,75 +1,103 @@
-"use client";
-import Link from "next/link";
-import { useAuthContext } from "./context/AuthContext";
-import { useEffect, useState } from 'react';
-import signOutUser from "@/firebase/auth/signout";
+import Image from "next/image";
 
 export default function Home() {
-  const { user, loading } = useAuthContext();
-  const [isClient, setIsClient] = useState(false);
-
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
-
-  const handleSignOut = async () => {
-    const { error } = await signOutUser();
-    if (error) {
-      console.error("Error al cerrar sesión:", error);
-    }
-  };
-
-  if (!isClient) {
-    return (
-      <div className="min-h-screen flex flex-col items-center justify-center bg-amarillo">
-        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-[#a13a4a]"></div>
-      </div>
-    );
-  }
-
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-amarillo">
-      <main className="flex flex-col items-center gap-8">
-        <h1 className="text-4xl font-bold text-burdeos drop-shadow-lg text-center">
-          Cartas sin marcar
-        </h1>
-        <p className="text-lg text-negroprofundo text-center max-w-md">
-          Un lugar donde enviar cartas a la antigua
-        </p>
-        <div className="flex gap-4 mt-8 flex-wrap justify-center">
-          {!loading && (user ? (
-            <>
-              <Link href="/cartas">
-                <button className="px-6 py-3 rounded-full bg-burdeos text-white font-bold shadow bg-[#a13a4a] transition cursor-pointer">
-                  Ver cartas disponibles
-                </button>
-              </Link>
-              <Link href="/nueva-carta">
-                <button className="px-6 py-3 rounded-full bg-amarillo text-burdeos font-bold shadow border border-burdeos bg-yellow-300 transition cursor-pointer">
-                  Agregar nueva carta
-                </button>
-              </Link>
-              <button
-                onClick={handleSignOut}
-                className="px-6 py-3 rounded-full bg-gray-300 text-gray-700 font-bold shadow transition cursor-pointer hover:bg-gray-400"
-              >
-                Cerrar sesión
-              </button>
-            </>
-          ) : (
-            <Link href="/signin">
-              <button className="px-6 py-3 rounded-full bg-burdeos text-white font-bold shadow bg-[#a13a4a] transition cursor-pointer">
-                Autenticarse
-              </button>
-            </Link>
-          ))}
-          <Link href="/tutorial">
-            <button className="px-6 py-3 rounded-full bg-[#bfa77a] text-white font-bold shadow transition cursor-pointer">
-              Ir al tutorial
-            </button>
-          </Link>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+          <li className="mb-2 tracking-[-.01em]">
+            Get started by editing{" "}
+            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
+              src/app/page.js
+            </code>
+            .
+          </li>
+          <li className="tracking-[-.01em]">
+            Save and see your changes instantly.
+          </li>
+        </ol>
+
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <a
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className="dark:invert"
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
+            Deploy now
+          </a>
+          <a
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Read our docs
+          </a>
         </div>
       </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to nextjs.org →
+        </a>
+      </footer>
     </div>
   );
 }
